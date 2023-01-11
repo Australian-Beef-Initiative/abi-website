2ec06fa4b8580694f734d1b6e2b1329f2c20a6e0
Chris Pollard <therealchrispollard@gmail.com>
2023-01-11 15:36:13 +1100

    mobile friendly menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,15 +39,13 @@ const theme = createTheme({
     MuiTypography: {
       defaultProps: {
         variantMapping: {
-          h1: 'h2',
+          h1: 'h1',
           h2: 'h2',
-          h3: 'h2',
-          h4: 'h2',
-          h5: 'h2',
-          h6: 'h2',
-          subtitle1: 'h2',
-          subtitle2: 'h2',
-          body1: 'span',
+          h3: 'h3',
+          h4: 'h4',
+          h5: 'h5',
+          h6: 'h6',
+          body1: 'p',
           body2: 'span',
         },
         fontFamily: [
