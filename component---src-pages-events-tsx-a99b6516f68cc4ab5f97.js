da1351ef840b52fe40c4bd702724c070737fe98f
Chris Pollard <therealchrispollard@gmail.com>
2023-01-13 17:17:59 +1100

    Updates

---
+++ component---src-pages-events-tsx-a99b6516f68cc4ab5f97.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkaustralian_beef_initiative=self.webpackChunkaustralian_beef_initiative||[]).push([[512],{2571:function(e,t,l){l.r(t);var n=l(7294),a=l(4102),i=l(1508),u=l(6063);const c={display:"flex",minHeight:"100vh",flexDirection:"column",justifyContent:"space-between"};t.default=()=>n.createElement(u.f6,null,n.createElement(a.Z,{sx:c,maxWidth:"lg"},n.createElement(i.Z,null,n.createElement(u.h4,null),n.createElement(u.zW,null)),n.createElement(u.$_,null)))}}]);
+//# sourceMappingURL=component---src-pages-events-tsx-a99b6516f68cc4ab5f97.js.map
